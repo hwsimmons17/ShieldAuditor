f847b3948350f575da7d380d19f2dde1ff728c28
Hunter Simmons <hunterwilliamsimmons@gmail.com>
2023-05-31 08:45:20 -0600

    Clean up search bar

---
+++ src/components/homeView.tsx
@@ -0,0 +1,11 @@
+export default function HomeView() {
+  return (
+    <>
+      <div className="border-b border-gray-200 bg-white px-4 py-5 sm:px-6">
+        <h3 className="text-base font-semibold leading-6 text-gray-900">
+          Home
+        </h3>
+      </div>
+    </>
+  );
+}
--- src/components/mainView.tsx
+++ src/components/mainView.tsx
@@ -7,6 +7,7 @@ import {
   MagnifyingGlassIcon,
 } from "@heroicons/react/20/solid";
 import Sidebar from "./sidebar";
+import HomeView from "./homeView";
 
 export default function MainView() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -112,63 +113,13 @@ export default function MainView() {
                     name="search"
                   />
                 </form>
-                <div className="flex items-center gap-x-4 lg:gap-x-6">
-                  <button
-                    type="button"
-                    className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500"
-                  >
-                    <span className="sr-only">View notifications</span>
-                    <BellIcon className="h-6 w-6" aria-hidden="true" />
-                  </button>
-
-                  {/* Separator */}
-                  <div
-                    className="hidden lg:block lg:h-6 lg:w-px lg:bg-gray-200"
-                    aria-hidden="true"
-                  />
-
-                  {/* Profile dropdown */}
-                  <Menu as="div" className="relative">
-                    <Menu.Button className="-m-1.5 flex items-center p-1.5">
-                      <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full bg-gray-50"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
-                      <span className="hidden lg:flex lg:items-center">
-                        <span
-                          className="ml-4 text-sm font-semibold leading-6 text-gray-900"
-                          aria-hidden="true"
-                        >
-                          Tom Cook
-                        </span>
-                        <ChevronDownIcon
-                          className="ml-2 h-5 w-5 text-gray-400"
-                          aria-hidden="true"
-                        />
-                      </span>
-                    </Menu.Button>
-                    <Transition
-                      as={Fragment}
-                      enter="transition ease-out duration-100"
-                      enterFrom="transform opacity-0 scale-95"
-                      enterTo="transform opacity-100 scale-100"
-                      leave="transition ease-in duration-75"
-                      leaveFrom="transform opacity-100 scale-100"
-                      leaveTo="transform opacity-0 scale-95"
-                    >
-                      <Menu.Items className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none"></Menu.Items>
-                    </Transition>
-                  </Menu>
-                </div>
               </div>
             </div>
           </div>
 
           <main className="py-10">
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-              {/* Your content */}
+              <HomeView />
             </div>
           </main>
         </div>
